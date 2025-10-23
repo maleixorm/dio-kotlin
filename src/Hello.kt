d1868b119f9e1534f519b6d59712fc57124dce6e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-10-23 18:55:12 -0300

    Olá mundo com comentários no código

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,3 +1,5 @@
-fun main(){
-    println("Hello World")
+package br.com.marcosaleixo.helloworld // Definição do pacote
+
+fun main(){                 // Ponto de entrada da função main
+    println("Hello World")  // Imprime a saída padrão, pulando uma linha
 }
