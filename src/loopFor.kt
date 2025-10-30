063dbaba96cd5bd124a2201d92266fc808031ec7
Marcos Aleixo <maknytz@gmail.com>
2025-10-30 11:44:14 -0300

    Trabalhando com a estrutura de repetição for no kotlin

---
+++ src/loopFor.kt
@@ -0,0 +1,8 @@
+package br.com.marcosaleixo.helloworld
+
+fun main() {
+    val cakes = listOf("carrot", "cheese", "chocolat")
+    for (cake in cakes) {
+        println("Yummy, it's a $cake cake!")
+    }
+}
