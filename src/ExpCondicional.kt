ddf26b48430b1b670ee4fc97ead702e0d1dfffdb
Marcos Aleixo <maknytz@gmail.com>
2025-10-30 14:51:19 -0300

    Trabalhando com Igualdade e Expresssão condicional no kotlin

---
+++ src/ExpCondicional.kt
@@ -0,0 +1,7 @@
+package br.com.marcosaleixo.helloworld
+
+fun main() {
+    fun max(a: Int, b: Int) = if (a > b) a else b
+
+    println(max(99, 142))
+}
---
+++ src/Igualdade.kt
@@ -0,0 +1,9 @@
+package br.com.marcosaleixo.helloworld
+
+fun main() {
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers)
+    println(authors === writers)
+}
